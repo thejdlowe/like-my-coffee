2f67d9b9d758dcbec48f2f6abf883d3e9834e4c9
J.D. Lowe <jdlowe.dev@gmail.com>
2024-07-21 01:33:35 -0400

    Code backups

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppContextProvider } from "./helpers/context";
 import { ControlPanel } from "./pages/ControlPanel";
 import { Scoreboard } from "./pages/Scoreboard";
+import { CssBaseline } from "@mui/material";
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
 	return (
 		<Router>
+			<CssBaseline />
 			<AppContextProvider>
 				<Routes>
 					<Route path="/" element={<Scoreboard />} />
--- client/src/pages/ControlPanel/ControlPanel.tsx
+++ client/src/pages/ControlPanel/ControlPanel.tsx
@@ -1,10 +1,12 @@
 import { useAppContext } from "../../helpers/context";
 import { scoreboardStates } from "../../../../shared/consts";
+import Grid from "@mui/material/Grid";
 import { useEffect } from "react";
 export const ControlPanel = () => {
-	const { gameState } = useAppContext();
+	/*const { gameState } = useAppContext();
 	useEffect(() => {
 		console.log(gameState);
-	}, [gameState]);
-	return <div>Fart</div>;
+	}, [gameState]);*/
+	//return <div>Fart</div>;
+	return (<Grid container direction="row" ></Grid>)
 };
