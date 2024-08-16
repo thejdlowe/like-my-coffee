24a2ae9b253d46f1c33bbde8971b6679be02090e
J.D. Lowe <jdlowe.dev@gmail.com>
2024-08-16 19:17:17 -0400

    Screensaver works!

--- client/src/pages/Scoreboard/components/Screensaver/Screensaver.tsx
+++ client/src/pages/Scoreboard/components/Screensaver/Screensaver.tsx
@@ -1,17 +1,55 @@
 import { useAppContext } from "../../../../helpers/context";
-import { useEffect } from "react";
-import Carousel from 'react-material-ui-carousel';
+import { useState, useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
 
 export const Screensaver = () => {
 	const { gameState } = useAppContext();
 	const { logo, social, apply, images } = gameState.fullShowData;
+	console.log(logo);
+	const [whatToRender, setWhatToRender] = useState<any>([]);
+
 	useEffect(() => {
-		console.log(gameState);
-	}, []);
-	return <center><img style={{
-		width: '100wh',
-		height: '95vh',
-		marginLeft: 'auto',
-		marginRight: 'auto',
-	}} src={logo} /></center>;
+		setWhatToRender(
+			<img
+				style={{
+					width: "100wh",
+					height: "95vh",
+					marginLeft: "auto",
+					marginRight: "auto",
+				}}
+				src={logo}
+			/>
+		);
+
+		const screensave = setTimeout(() => {
+			setWhatToRender([
+				<Carousel
+					indicators={false}
+					interval={10000}
+					autoPlay={true}
+					swipe={false}
+					animation="fade"
+					duration={2000}
+					stopAutoPlayOnHover={false}
+				>
+					{images.map((el, index) => (
+						<img
+							key={index}
+							style={{
+								width: "100wh",
+								height: "95vh",
+								marginLeft: "auto",
+								marginRight: "auto",
+							}}
+							src={el}
+						/>
+					))}
+				</Carousel>,
+			]);
+		}, 6000);
+		return () => {
+			clearTimeout(screensave);
+		};
+	}, [logo]);
+	return <center>{whatToRender}</center>;
 };
