a0bcf0fd43b6ab29bbf38fc249519c8816a9a53b
J.D. Lowe <jdlowe.dev@gmail.com>
2024-09-07 13:02:46 -0400

    Final round styling

--- client/src/pages/Scoreboard/components/FinalRound/FinalRound.tsx
+++ client/src/pages/Scoreboard/components/FinalRound/FinalRound.tsx
@@ -1,4 +1,4 @@
-import { useAppContext } from "../../../../helpers/context";
+import { Stack, Box, Typography } from "@mui/material";
 export const FinalRound = () => {
-	return <>Final Round</>;
+	return <Stack><center><Typography variant="h1">FINAL ROUND</Typography></center></Stack>
 };
