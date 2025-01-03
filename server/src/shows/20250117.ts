1cb77c5d375088345f22e114ff1345ec4c0e60ce
J.D. Lowe <jdlowe.dev@gmail.com>
2025-01-03 18:52:07 -0500

    We are ready for the January show!

---
+++ server/src/shows/20250117.ts
@@ -0,0 +1,99 @@
+import { ShowType } from "../../sharedCopy";
+
+export const show: ShowType = {
+	rounds: [
+		{
+			players: [
+				{
+					displayName: "Aaron",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 0,
+				},
+				{
+					displayName: "Karissa",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 1,
+				},
+				{
+					displayName: "Kelley",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 2,
+				},
+			],
+			minigame: "We've Got Movie Line",
+			example:
+				"Sex with me is like the movie Aladdin: the sequel will be nowhere near as good as the original.",
+		},
+		{
+			players: [
+				{
+					displayName: "Kal",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 3,
+				},
+				{
+					displayName: "Zach",
+					pronouns: "",
+					score: 0,
+					soundIndex: 4,
+				},
+				{
+					displayName: "Des",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 5,
+				},
+			],
+			minigame: "Name That Position",
+			example:
+				"The USB Drive - You have to flip over twice before it can go in",
+		},
+		{
+			players: [
+				{
+					displayName: "Jen",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 6,
+				},
+				{
+					displayName: "Ervin",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 7,
+				},
+				{
+					displayName: "Kelsey",
+					pronouns: "She/He/They",
+					score: 0,
+					soundIndex: 8,
+				},
+			],
+			minigame: "Bad Communication",
+			example: "Doing the laundry - you mean I have to put ANOTHER load in?",
+		},
+	],
+	images: [
+		"Apply.png",
+		"Aaron.png",
+		"Des.png",
+		"Ervin.png",
+		"Jennifer.png",
+		"JD.png",
+		"Justin.png",
+		"Kal.png",
+		"Karissa.png",
+		"Kelley.png",
+		"Kelsey.png",
+		"Zach.png",
+		"Adam.png",
+		"Social.png",
+	],
+	logo: "logo.png",
+	apply: "Apply.png",
+	social: "Social.png",
+};
