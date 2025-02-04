1050539784990adef24cc0c40a7bd31914b02d15
J.D. Lowe <jdlowe.dev@gmail.com>
2025-02-04 17:01:03 -0500

    Tiny

--- client/src/pages/PlayerDisplay/PlayerDisplay.tsx
+++ client/src/pages/PlayerDisplay/PlayerDisplay.tsx
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router';
+import { DefaultPlayerDisplay } from './components';
 
 export const PlayerDisplay = () => {
     const {id} = useParams();
-    if(!id) return(<></>);
+    if(!id) return(<DefaultPlayerDisplay />);
     return (<>{id}</>)
 }
