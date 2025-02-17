42d8257d2b06e2184819ac1282b3181e4450ff10
J.D. Lowe <jdlowe.dev@gmail.com>
2025-02-16 23:30:06 -0500

    More controller backup

--- controller/code.py
+++ controller/code.py
@@ -1,31 +1,32 @@
 ### print("Hello World!")
-import board, time, digitalio # board dependency
+import board, time # board dependency
+from digitalio import DigitalInOut, Direction, Pull
 import os
 import ssl
 import adafruit_requests
 import microcontroller
-import wifi
+# import wifi
 import socketpool
+from adafruit_debouncer import Button
 
 print("Starting")
+light_number_redcable = 13
+
+button_pin = DigitalInOut(board.GP18)
+button_pin.direction = Direction.INPUT
+button_pin.pull = Pull.DOWN
+button = Button(button_pin, long_duration_ms=5000, value_when_pressed=True)
 
 try:
+    '''
     print("my IP addr:", wifi.radio.ipv4_address)
     
     pool = socketpool.SocketPool(wifi.radio)
     requests = adafruit_requests.Session(pool, ssl.create_default_context())
-    CONTROLLER_NUMBER = os.getenv('CONTROLLER_NUMBER')
-    response = requests.get(f'http://likemycoffee.local:3001/buzz/{CONTROLLER_NUMBER}/100')
-    print("Text response: ", response.text)
     '''
-    pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
-
-    ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
-
-    requests = adafruit_requests.Session(pool, ssl_context)
-
-    with requests.get(f'http://likemycoffee.local:3001/buzz/{CONTROLLER_NUMBER}/100') as response:
-        print(f" | ✅ Unparsed Full JSON Response: {response.json()}")
+    CONTROLLER_NUMBER = os.getenv('CONTROLLER_NUMBER')
+    # response = requests.get(f'http://likemycoffee.local:3001/buzz/{CONTROLLER_NUMBER}/100')
+    # print("Text response: ", response.text)
     '''
     # Setup LED
     led = digitalio.DigitalInOut(board.LED)
@@ -42,13 +43,17 @@ try:
             
 
     # Flash the external LED 5 times rapidly
-    flash_led(100, 0.1)
+    # flash_led(100, 0.1)
+    '''
+    print("Whee")
+    while True:
+        button.update()
+        if button.short_count:
+            print("Short")
+        if button.long_press:
+            print("Long")
 except Exception as e:
     print("Error:\n", str(e))
     print("Resetting microcontroller in 10 seconds")
     time.sleep(10)
     microcontroller.reset()
-
-
-
-
