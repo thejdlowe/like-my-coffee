225b8992387778166efb37433d8568bd484345e7
J.D. Lowe <jdlowe.dev@gmail.com>
2025-02-21 10:43:20 -0500

    Updating heights

--- client/src/pages/Scoreboard/components/CreditsScreen/CreditsScreen.tsx
+++ client/src/pages/Scoreboard/components/CreditsScreen/CreditsScreen.tsx
@@ -1,7 +1,7 @@
 export const CreditsScreen = () => {
 	return (
 		<center>
-			<video autoPlay>
+			<video autoPlay style={{height: "100vh"}}>
 				<source src="credits.mp4" type="video/mp4" />
 			</video>
 		</center>
--- client/src/pages/Scoreboard/components/MysteryScreen/MysteryScreen.tsx
+++ client/src/pages/Scoreboard/components/MysteryScreen/MysteryScreen.tsx
@@ -1,7 +1,7 @@
 export const MysteryScreen = () => {
 	return (
 		<center>
-			<video autoPlay>
+			<video autoPlay style={{height: "100vh"}}>
 				<source src="surprise.mp4" type="video/mp4" />
 			</video>
 		</center>
