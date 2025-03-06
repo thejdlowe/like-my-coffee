612ab5d90931dd2b26a51b65b43d0e4bcb9b729b
J.D. Lowe <jdlowe.dev@gmail.com>
2025-03-05 22:46:37 -0500

    Tiny detail that should have been squashed lol

--- controller/index.js
+++ controller/index.js
@@ -1,44 +1,49 @@
-const noble = require('@abandonware/noble');
+const noble = require("@abandonware/noble");
 
-noble.on('stateChange', function (state) {
-    if (state === 'poweredOn') {
-        noble.startScanning();
-    }
-    else {
-        noble.stopScanning();
-    }
+noble.on("stateChange", function (state) {
+	if (state === "poweredOn") {
+		noble.startScanning();
+	} else {
+		noble.stopScanning();
+	}
 });
 
-console.log("Scanning for Bluetooth")
+console.log("Scanning for Bluetooth");
 
-noble.on('discover', async function (device) {
-    const mac = device.address; // retrieves the MAC address
-    const goodMacs = ['D8:3A:DD:76:3D:40'];
-    if (goodMacs.includes(mac.toUpperCase())) {
-        console.log(`${mac} discovered`);
-        await device.connectAsync();
-        console.log(`${mac} connected, getting services`);
+noble.on("discover", async function (device) {
+	const mac = device.address; // retrieves the MAC address
+	const goodMacs = ["D8:3A:DD:76:3D:40"];
+	if (goodMacs.includes(mac.toUpperCase())) {
+		console.log(`${mac} discovered`);
+		await device.connectAsync();
+		console.log(`${mac} connected, getting services`);
 
-        device.discoverAllServicesAndCharacteristics((err, services, characteristics) => {
-            //https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf?v=1740981361600
+		console.log(device);
 
-            characteristics.forEach((characteristic) => {
-                if (characteristic.uuid === "2a6f") {
-                    console.log(`Monitoring characteristic ${characteristic.uuid}`);
-                    let lastdata = null;
+		device.discoverAllServicesAndCharacteristics(
+			(err, services, characteristics) => {
+				//https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf?v=1740981361600
 
-                    setInterval(async () => {
-                        const newdata = (await characteristic.readAsync());
-                        if (lastdata !== newdata.toString()) {
-                            console.log("Data received from controler: ", newdata.toString());
-                            lastdata = newdata.toString();
-                            const controllernumber = lastdata.split("&")[0];
-                            fetch(`http://likemycoffee.local:3001/buzz/${controllernumber}`)
+				characteristics.forEach((characteristic) => {
+					if (characteristic.uuid === "2a6f") {
+						console.log(`Monitoring characteristic ${characteristic.uuid}`);
+						let lastdata = null;
 
-                        }
-                    }, 100)
-                }
-            })
-        });
-    }
+						setInterval(async () => {
+							const newdata = await characteristic.readAsync();
+							if (lastdata !== newdata.toString()) {
+								console.log(
+									"Data received from controler: ",
+									newdata.toString()
+								);
+								lastdata = newdata.toString();
+								const controllernumber = lastdata.split("&")[0];
+								fetch(`http://localhost:3001/buzz/${controllernumber}`);
+							}
+						}, 100);
+					}
+				});
+			}
+		);
+	}
 });
