32cf05f7321fe6773bbe505e08fce67efa4b748c
J.D. Lowe <jdlowe.dev@gmail.com>
2025-03-08 15:37:29 -0500

    More troubleshooting

--- controller/index.js
+++ controller/index.js
@@ -13,6 +13,14 @@ noble.on("stateChange", async function (state) {
 	}
 });
 
+noble.on("error", () => {
+	console.log(arguments)
+})
+
+noble.on('warning', (message) => {
+	console.log(message)
+});
+
 console.log("Scanning for Bluetooth");
 
 noble.on("discover", async function (device) {
@@ -22,13 +30,15 @@ noble.on("discover", async function (device) {
 		console.log(`${mac} discovered`);
 		await noble.stopScanningAsync();
 		console.log(`Scanning stopped`);
+
 		await device.connectAsync();
-		console.log("Sleep");
-		await sleep(3000);
+		console.log("Sleep for 5 seconds");
+		await sleep(5000);
 		console.log(`${mac} connected, getting services`);
 
 		device.discoverAllServicesAndCharacteristics(
 			async (err, services, characteristics) => {
+				console.log(`Services found for ${mac}`)
 				//https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf?v=1740981361600
 				await noble.startScanningAsync();
 
