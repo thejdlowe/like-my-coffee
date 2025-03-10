3b2e4cde77cac95624fb97b1890c13f8408e08ba
J.D. Lowe <jdlowe.dev@gmail.com>
2025-03-10 00:07:12 -0400

    Removing confirms

--- client/src/pages/ControlPanel/components/TopBar/components/PowerFunctionsHolder/PowerFunctionsHolder.tsx
+++ client/src/pages/ControlPanel/components/TopBar/components/PowerFunctionsHolder/PowerFunctionsHolder.tsx
@@ -2,10 +2,12 @@ import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import { useState } from "react";
 import { Button } from "@mui/material";
 export const PowerFunctionsHolder = () => {
-	const [showModal, setShowModal] = useState<boolean>(false);
+	const [showMainModal, setShowMainModal] = useState<boolean>(false);
+	const [showRebootModal, setShowRebootModal] = useState<boolean>(false);
+	const [showShutdownModal, setShowShutdownModal] = useState<boolean>(false);
 	return (
 		<>
-			{showModal && (
+			{showRebootModal && (
 				<div
 					style={{
 						position: "absolute",
@@ -17,7 +19,75 @@ export const PowerFunctionsHolder = () => {
 						display: "flex",
 					}}
 					onClick={() => {
-						setShowModal(false);
+						setShowRebootModal(false);
+					}}
+				>
+					<div
+						style={{
+							width: "50%",
+							height: "30%",
+							backgroundColor: "White",
+						}}
+					>
+						<Button
+							onClick={() => {
+								fetch(`http://likemycoffee.local:3001/forcerebootnowdangit/`, {
+									method: "GET",
+								});
+							}}
+						>
+							Reboot For Reals
+						</Button>
+					</div>
+				</div>
+			)}
+			{showShutdownModal && (
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						backgroundColor: "rgba(0, 0, 0, 0.3)",
+						display: "flex",
+					}}
+					onClick={() => {
+						setShowShutdownModal(false);
+					}}
+				>
+					<div
+						style={{
+							width: "50%",
+							height: "30%",
+							backgroundColor: "White",
+						}}
+					>
+						<Button
+							onClick={() => {
+								fetch(`http://likemycoffee.local:3001/forceshutdownawwwman/`, {
+									method: "GET",
+								});
+							}}
+						>
+							Power Off For Reals
+						</Button>
+					</div>
+				</div>
+			)}
+			{showMainModal && (
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						backgroundColor: "rgba(0, 0, 0, 0.3)",
+						display: "flex",
+					}}
+					onClick={() => {
+						setShowMainModal(false);
 					}}
 				>
 					<div
