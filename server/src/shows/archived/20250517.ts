b2178230f413b7bb013f42a1031590730ca15523
J.D. Lowe <jdlowe.dev@gmail.com>
2025-06-06 22:58:44 -0400

    Prepping for 06/21/2025 show

---
+++ server/src/shows/archived/20250517.ts
@@ -0,0 +1,138 @@
+import { ShowType } from "../../../sharedCopy";
+
+export const show: ShowType = {
+	rounds: [
+		{
+			players: [
+				{
+					displayName: "Kal",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 0,
+					isWinner: false,
+				},
+				{
+					displayName: "Indy",
+					pronouns: "They/Them",
+					score: 0,
+					soundIndex: 1,
+					isWinner: false,
+				},
+				{
+					displayName: "Rina",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 2,
+					isWinner: false,
+				},
+			],
+			minigame: "Dating Apps",
+			example: "Facebook: It's been a while since someone's poked me",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "Lindy",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 3,
+					isWinner: false,
+				},
+				{
+					displayName: "Rico",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 4,
+					isWinner: false,
+				},
+				{
+					displayName: "Pinak",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 5,
+					isWinner: false,
+				},
+			],
+			minigame: "Sexy Slogans",
+			example: "Nintendo: Looking for a new Switch",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "Jeremy",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 6,
+					isWinner: false,
+				},
+				{
+					displayName: "Kevin",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 7,
+					isWinner: false,
+				},
+				{
+					displayName: "Leslie",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 8,
+					isWinner: false,
+				},
+			],
+			minigame: "Communication Breakdown",
+			example:
+				"Doing the laundry - Yeah I'm OK to put another load in before going to bed.",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 9,
+					isWinner: false,
+				},
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 10,
+					isWinner: false,
+				},
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 11,
+					isWinner: false,
+				},
+			],
+			minigame: "",
+			example: "",
+			timelength: 10,
+		},
+	],
+	images: [
+		"Apply.png",
+		"Jeremy.png",
+		"Kal.png",
+		"Kevin.png",
+		"Leslie.png",
+		"Lindy.png",
+		"JD.png",
+		"Justin.png",
+		"Indy.png",
+		"Pinak.png",
+		"Rico.png",
+		"Rina.png",
+		"Adam.png",
+		"Social.png",
+	],
+	logo: "logo.png",
+	apply: "Apply.png",
+	social: "Social.png",
+};
--- server/src/shows/index.ts
+++ server/src/shows/index.ts
@@ -1,2 +1,2 @@
 //Update where this points to before every new show!
-export * from "./20250517";
+export * from "./20250621";
