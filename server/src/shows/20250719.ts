f50a3e048a786d91eb966082f009e68fbea71468
J.D. Lowe <jdlowe.dev@gmail.com>
2025-07-09 13:15:20 -0400

    Prepping for 07/19 show

---
+++ server/src/shows/20250719.ts
@@ -0,0 +1,136 @@
+import { ShowType } from "../../sharedCopy";
+
+export const show: ShowType = {
+	rounds: [
+		{
+			players: [
+				{
+					displayName: "Justin",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 0,
+					isWinner: false,
+				},
+				{
+					displayName: "Kieran",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 1,
+					isWinner: false,
+				},
+				{
+					displayName: "Mooshe",
+					pronouns: "She/They",
+					score: 0,
+					soundIndex: 2,
+					isWinner: false,
+				},
+			],
+			minigame: "Name That Position",
+			example: "USB Drive: You flip it over twice before it goes in right",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "Mike",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 3,
+					isWinner: false,
+				},
+				{
+					displayName: "Lisa",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 4,
+					isWinner: false,
+				},
+				{
+					displayName: "Tony",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 5,
+					isWinner: false,
+				},
+			],
+			minigame: "Sexy Slogans",
+			example: "Nintendo: Looking for a new Switch",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "John",
+					pronouns: "They/Them",
+					score: 0,
+					soundIndex: 6,
+					isWinner: false,
+				},
+				{
+					displayName: "Audrey",
+					pronouns: "She/Her",
+					score: 0,
+					soundIndex: 7,
+					isWinner: false,
+				},
+				{
+					displayName: "ThaGawdShow",
+					pronouns: "He/Him",
+					score: 0,
+					soundIndex: 8,
+					isWinner: false,
+				},
+			],
+			minigame: "Communication Breakdown",
+			example:
+				"Doing the laundry - Yeah I'm OK to put another load in before going to bed.",
+			timelength: 12,
+		},
+		{
+			players: [
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 9,
+					isWinner: false,
+				},
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 10,
+					isWinner: false,
+				},
+				{
+					displayName: "Dummy Data",
+					pronouns: "",
+					score: 0,
+					soundIndex: 11,
+					isWinner: false,
+				},
+			],
+			minigame: "",
+			example: "",
+			timelength: 10,
+		},
+	],
+	images: [
+		"Apply.png",
+		"Audrey.png",
+		"John.png",
+		"Justin.png",
+		"Kieran.png",
+		"JD.png",
+		"Lisa.png",
+		"Mike.png",
+		"ThaGawdShow.png",
+		"Tony.png",
+		"Adam.png",
+		"Social.png",
+	],
+	logo: "logo.png",
+	apply: "Apply.png",
+	social: "Social.png",
+};
