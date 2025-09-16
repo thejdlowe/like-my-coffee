c28c2ee192759d3153f2febf19bfe7e0e8085dda
J.D. Lowe <jdlowe.dev@gmail.com>
2025-09-15 23:35:45 -0400

    Nametag generator mostly done!

--- client/src/pages/Nametags/Nametags.tsx
+++ client/src/pages/Nametags/Nametags.tsx
@@ -1,32 +1,62 @@
-import { Stack, Typography, Box } from "@mui/material";
 import { useAppContext } from "../../helpers/context";
+import { useEffect } from "react";
 import "./nametags.css";
 
 export const Nametags = () => {
 	const { serverState: gameState } = useAppContext();
-	//const players = [gameState.fullShowData.rounds[0].players]
 	const { fullShowData } = gameState;
 	const { rounds } = fullShowData;
-	let players: any = [];
-	if (rounds.length) {
-		players = [
-			...rounds[0].players,
-			...rounds[1].players,
-			...rounds[2].players,
-		];
-	}
-
-	console.log(players);
-	//const players = [...rounds[0].players]
+	useEffect(() => {
+		document.title = "Nametag Generator";
+	}, []);
 	return (
 		<>
-			{players.map((player: any) => {
+			{rounds.length &&
+				[rounds[0], rounds[1], rounds[2]].map((round, index) => {
+					return (
+						<>
+							<div className="page">
+								{round.players.map((player: any) => {
+									return (
+										<div className="nametag" contentEditable="true">
+											<div className="nametag-lmc-logo"> </div>
+											<div className="nametag-name">{player.displayName}</div>
+											<div className="nametag-pronouns">{player.pronouns}</div>
+											<div className="nametag-mfp-logo"> </div>
+										</div>
+									);
+								})}
+							</div>
+							<div className="page">
+								{round.players.map((player: any, playerIndex: number) => {
+									const playerNumber = playerIndex + 1;
+									let color = "";
+									if (playerNumber === 1) color = "green";
+									if (playerNumber === 2) color = "red";
+									if (playerNumber === 3) color = "yellow";
+									return (
+										<div className="nametag column" contentEditable="true">
+											<div className="nametag-name">Round: {index + 1}</div>
+											<div className="nametag-name">Mug Color: {color}</div>
+											<div className="nametag-name">
+												Mini Game:<br />{round.minigame}
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						</>
+					);
+				})}
+			{[0, 1].map((el) => {
 				return (
-					<div className="nametag">
-						<div className="nametag-lmc-logo"> </div>
-						<div className="nametag-name">{player.displayName}</div>
-						<div className="nametag-pronouns">{player.pronouns}</div>
-						<div className="nametag-mfp-logo"></div>
+					<div className="page">
+						<div className="nametag" contentEditable="true">
+							<div className="nametag-lmc-logo"> </div>
+							<div className="nametag-name">Indy</div>
+							<div className="nametag-pronouns">They/Them</div>
+							<div className="nametag-mfp-logo"> </div>
+						</div>
 					</div>
 				);
 			})}
